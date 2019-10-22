5dec098989a89c4c8434b3917a92c32b5fd71a6d
Rody Davis <rody.davis.jr@gmail.com>
2019-10-22 14:37:05 -0400

    updating

--- lib/json_widget.dart
+++ lib/json_widget.dart
@@ -3,9 +3,15 @@
 // found in the LICENSE file.
 
 import 'package:build/build.dart' as build;
+import 'package:flutter/material.dart';
+import 'package:json_widget/src/widgets/builder.dart';
+
 import 'src/builder.dart';
 export 'src/widgets/builder.dart';
 
 /// Creates a [JsonWidgetBuilder]
-build.Builder jsonWidgetBuilder(build.BuilderOptions builderOptions) =>
-    const JsonWidgetBuilder();
+build.Builder jsonWidgetBuilder(build.BuilderOptions builderOptions) {
+  return const JsonWidgetBuilder();
+}
+
+Widget jsonRuntimeWidget(Map<String, dynamic> data) => JsonWidget(data: data);
